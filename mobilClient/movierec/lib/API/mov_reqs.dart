39d0d7bfc5eaa773aee706cbd9c8226cb434cbc9
MahmoudElshrif <mahmoudfathyelshrif@gmail.com>
2025-12-09 22:20:37 +0200

    search bar

--- mobilClient/movierec/lib/API/mov_reqs.dart
+++ mobilClient/movierec/lib/API/mov_reqs.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:movierec/model/recommend_result.dart';
 import 'package:movierec/model/search_result.dart';
 
 class Requests {
-  static Future<List<SearchResult>> fetchSearch(String title) async {
+  static Future<List<SearchResult>> searchFor(String title) async {
     final response = await http.get(
-      Uri.parse('http://192.168.100.34:5000/search?q=$title'),
+      Uri.parse('http://192.168.1.12:5000/search?q=$title'),
     );
 
     if (response.statusCode == 200) {
@@ -14,6 +15,21 @@ class Requests {
 
       // Convert each JSON map into your model
       return jsonList.map((json) => SearchResult.fromJson(json)).toList();
+    } else {
+      throw Exception("Failed to load search results");
+    }
+  }
+
+  static Future<List<RecommendResult>> getRecommendations(String title) async {
+    final response = await http.get(
+      Uri.parse('http://192.168.1.12:5000/recommendation?title=$title'),
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonList = jsonDecode(response.body);
+
+      // Convert each JSON map into your model
+      return jsonList.map((json) => RecommendResult.fromJson(json)).toList();
     } else {
       throw Exception("Failed to load search results");
     }
